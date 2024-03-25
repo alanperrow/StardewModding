7371d033e42b40c72bb39ab25ee26cab37d89ac9
Alan Perrow <65362286+alanperrow@users.noreply.github.com>
2024-03-25 19:00:05 -0400

    Initial research into splitscreen music bug. Took notes in SDV mod notes txt.

---
+++ SplitscreenMusicFix/Patches.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.GameData;
+
+namespace SplitscreenMusicFix
+{
+    [HarmonyPatch(typeof(Game1))]
+    public class Patches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(Game1.changeMusicTrack))]
+        public static bool ChangeMusicTrack_Prefix(string newTrackName, bool track_interruptable, MusicContext music_context)
+        {
+            //var debug_Game1 = Game1.game1;
+
+            //var sf = new StackFrame();
+            //StackTrace st = new StackTrace(1, true);
+            //StackFrame stFrame1 = st.GetFrame(1); // prev frame
+            //string invkMethodName = stFrame1.GetMethod().Name;
+            string invkMethodName = string.Empty;
+
+            string pref1 = Game1.player.IsMainPlayer ? "P1: " : "P2: ";
+
+            ModEntry.Instance.Monitor.Log(
+                //$"newTrackName={newTrackName ?? "NULL"}\t\ttrack_interruptable={track_interruptable}\t\tmusic_context={music_context}\t\t(tick={Game1.ticks})",
+                 pref1 + $"{newTrackName ?? "NULL"}\t{track_interruptable}\t{music_context}\t({Game1.ticks})\t{invkMethodName}",
+                LogLevel.Debug);
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                ModEntry.Instance.Monitor.Log($"Failed in {nameof(ChangeMusicTrack_Prefix)}:\n{e}", LogLevel.Error);
+            }
+
+            return true;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Game1.changeMusicTrack))]
+        public static void ChangeMusicTrack_Postfix(string newTrackName, bool track_interruptable, MusicContext music_context)
+        {
+            //var debug_Game1 = Game1.game1;
+            //ModEntry.Instance.Monitor.Log(
+            //    $"newTrackName={newTrackName ?? "NULL"}\t\ttrack_interruptable={track_interruptable}\t\tmusic_context={music_context}",
+            //    LogLevel.Debug);
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                ModEntry.Instance.Monitor.Log($"Failed in {nameof(ChangeMusicTrack_Postfix)}:\n{e}", LogLevel.Error);
+            }
+        }
+    }
+}
