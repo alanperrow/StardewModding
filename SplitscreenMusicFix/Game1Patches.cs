bb5249b779abadd667431055458f96b3519e9db9
Alan Perrow <65362286+alanperrow@users.noreply.github.com>
2024-03-25 20:36:04 -0400

    First attempt at Game1.SetWindowSize transpiler. Almost works, but fails after multiple calls in-game.

--- SplitscreenMusicFix/Game1Patches.cs
+++ SplitscreenMusicFix/Game1Patches.cs
@@ -1,17 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Diagnostics;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
 using StardewModdingAPI;
 using StardewValley;
 using StardewValley.GameData;
-
 
 namespace BetterSplitscreen
 {
     [HarmonyPatch(typeof(Game1))]
     public class Game1Patches
     {
-        // Music bug - still investigating
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(Game1.SetWindowSize))]
+        [HarmonyPatch(new Type[] { typeof(int), typeof(int) })]
+        public static IEnumerable<CodeInstruction> SetWindowSize_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo getScreenSplitsMethod = AccessTools.Method(typeof(ModLogic), nameof(ModLogic.GetScreenSplits));
+            //MethodInfo getScreenSplitsMethod = AccessTools.Method(typeof(ModLogic), nameof(DebugLog1));
+
+            List<CodeInstruction> instructionsList = instructions.ToList();
+
+            bool foundInstruction = false;
+
+            for (int i = 0; i < instructionsList.Count; i++)
+            {
+                // Find instruction: `if (GameRunner.instance.gameInstances.Count <= 1)`
+                // IL_02dc (instructionsList[182])
+                if (i > 0 && i < instructionsList.Count - 1
+                    && instructionsList[i - 1].opcode == OpCodes.Callvirt // && operand is method {Void Add(Microsoft.Xna.Framework.Vector4}
+                    && instructionsList[i].opcode == OpCodes.Ldsfld
+                    && instructionsList[i + 1].opcode == OpCodes.Ldfld)
+                {
+                    //yield return new CodeInstruction(OpCodes.Ldloc_1); // load local List<Vector4> `screen_splits`
+
+                    yield return new CodeInstruction(OpCodes.Call, getScreenSplitsMethod);  // call GetScreenSplits()
+                    yield return new CodeInstruction(OpCodes.Stloc_1);                      // store result in local List<Vector4> `screen_splits` (this overwrites base game values)
+
+                    foundInstruction = true;
+                }
+
+                yield return instructionsList[i];
+            }
+
+            if (!foundInstruction)
+            {
+                ModEntry.Instance.Monitor.Log(
+                    $"{nameof(SetWindowSize_Transpiler)} could not find target instruction(s) in {nameof(Game1.SetWindowSize)}, so no changes were made.",
+                    LogLevel.Error);
+            }
+
+            yield break;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(Game1.SetWindowSize))]
+        public static bool SetWindowSize_Prefix(Game1 __instance, int w, int h)
+        {
+
+            var window = __instance.localMultiplayerWindow;
+            var player = Game1.player;
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                ModEntry.Instance.Monitor.Log($"Failed in {nameof(SetWindowSize_Prefix)}:\n{e}", LogLevel.Error);
+            }
+
+            return true;
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(Game1.SetWindowSize))]
+        public static void SetWindowSize_Postfix(Game1 __instance, int w, int h)
+        {
+            var window = __instance.localMultiplayerWindow;
+            var player = Game1.player;
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                ModEntry.Instance.Monitor.Log($"Failed in {nameof(SetWindowSize_Postfix)}:\n{e}", LogLevel.Error);
+            }
+        }
+
+
+
+
+        // TODO: Music bug - still investigating
         /*
         [HarmonyPrefix]
         [HarmonyPatch(nameof(Game1.changeMusicTrack))]
---
+++ SplitscreenMusicFix/ModLogic.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Xna.Framework;
+using StardewValley;
+using System.Collections.Generic;
+
+namespace BetterSplitscreen
+{
+    public static class ModLogic
+    {
+        /// <summary>
+        /// Calculates the screen split locations for each splitscreen player.
+        /// TODO: , dependent on the selected layout from ModConfig.
+        /// </summary>
+        /// <returns>The list of screen split locations.</returns>
+        public static List<Vector4> GetScreenSplits()
+        {
+            // TODO: Conditional logic based on ModConfig.
+            // IDEA: Make a nice pretty graphic with red/blue/green/yellow boxes representing each individual splitscreen position.
+            bool isDefaultLayout = false;
+
+            List<Vector4> screenSplits = new();
+
+            if (isDefaultLayout)
+            {
+                // DEFAULT GAME LAYOUT
+                if (GameRunner.instance.gameInstances.Count <= 1)
+                {
+                    screenSplits.Add(new Vector4(0f, 0f, 1f, 1f));
+                }
+                else
+                {
+                    switch (GameRunner.instance.gameInstances.Count)
+                    {
+                        case 2:
+                            screenSplits.Add(new Vector4(0f, 0f, 0.5f, 1f));
+                            screenSplits.Add(new Vector4(0.5f, 0f, 0.5f, 1f));
+                            break;
+                        case 3:
+                            screenSplits.Add(new Vector4(0f, 0f, 1f, 0.5f));
+                            screenSplits.Add(new Vector4(0f, 0.5f, 0.5f, 0.5f));
+                            screenSplits.Add(new Vector4(0.5f, 0.5f, 0.5f, 0.5f));
+                            break;
+                        case 4:
+                            screenSplits.Add(new Vector4(0f, 0f, 0.5f, 0.5f));
+                            screenSplits.Add(new Vector4(0.5f, 0f, 0.5f, 0.5f));
+                            screenSplits.Add(new Vector4(0f, 0.5f, 0.5f, 0.5f));
+                            screenSplits.Add(new Vector4(0.5f, 0.5f, 0.5f, 0.5f));
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                // CUSTOM LAYOUT
+                if (GameRunner.instance.gameInstances.Count <= 1)
+                {
+                    screenSplits.Add(new Vector4(0f, 0f, 1f, 1f));
+                }
+                else
+                {
+                    switch (GameRunner.instance.gameInstances.Count)
+                    {
+                        case 2:
+                            screenSplits.Add(new Vector4(0.5f, 0f, 0.5f, 1f));
+                            screenSplits.Add(new Vector4(0f, 0f, 0.5f, 1f));
+                            break;
+                        case 3:
+                            screenSplits.Add(new Vector4(0f, 0.5f, 0.5f, 0.5f));
+                            screenSplits.Add(new Vector4(0f, 0f, 1f, 0.5f));
+                            screenSplits.Add(new Vector4(0.5f, 0.5f, 0.5f, 0.5f));
+                            break;
+                        case 4:
+                            screenSplits.Add(new Vector4(0.5f, 0f, 0.5f, 0.5f));
+                            screenSplits.Add(new Vector4(0f, 0f, 0.5f, 0.5f));
+                            screenSplits.Add(new Vector4(0.5f, 0.5f, 0.5f, 0.5f));
+                            screenSplits.Add(new Vector4(0f, 0.5f, 0.5f, 0.5f));
+                            break;
+                    }
+                }
+            }
+
+            return screenSplits;
+        }
+    }
+}
