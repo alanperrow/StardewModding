f30f8058fbad0d261b72d9467a5403110da2ffbf
Alan Perrow <65362286+alanperrow@users.noreply.github.com>
2024-04-12 13:31:21 -0400

    Check main instance when calling from splitscreen instance for IsMusicContextActiveButNotPlaying.

--- BetterSplitscreen/MusicFix/MusicFixHelper.cs
+++ BetterSplitscreen/MusicFix/MusicFixHelper.cs
@@ -5,6 +5,7 @@ using StardewValley;
 using StardewValley.GameData;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace SplitscreenImproved.MusicFix
@@ -13,7 +14,9 @@ namespace SplitscreenImproved.MusicFix
     {
         internal static void DrawDebugText(SpriteBatch sb)
         {
-            string playerNum = $"P{Game1.game1.instanceIndex + 1}: ";
+            Game1 thisInstance = Game1.game1;
+
+            string playerNum = $"P{thisInstance.instanceIndex + 1}: ";
             string agg = string.Empty;
             foreach (MusicContext musicContext in Enum.GetValues<MusicContext>())
             {
@@ -21,10 +24,19 @@ namespace SplitscreenImproved.MusicFix
                 agg += playerNum + musicContext.ToString() + ": " + key + "\n";
             }
 
-            // DEBUG
+            agg += "--------------\n";
+
+            MusicContext activeMusicContext = (MusicContext)instanceActiveMusicContextField.GetValue(thisInstance);
+            agg += playerNum + "ActiveMusicContext: " + activeMusicContext.ToString() + "\n";
+
             agg += playerNum + "IsMusicContextActiveButNotPlaying(): " + IsMusicContextActiveButNotPlaying(MusicContext.Default).ToString() + "\n";
-            // DEBUG
-            
+
+            Game1 mainInstance = GameRunner.instance.gameInstances.Single(x => x.IsMainInstance);
+            string mainCurrentTrack = GetMusicTrackNameOfInstance(mainInstance, MusicContext.Default);
+            agg += playerNum + "Main music track: " + mainCurrentTrack + "\n";
+            string mainCurrentTrackSplitscreen = GetMusicTrackNameOfInstance(mainInstance, MusicContext.ImportantSplitScreenMusic);
+            agg += playerNum + "Main splitscreen music track: " + mainCurrentTrackSplitscreen + "\n";
+
             sb.DrawString(Game1.smallFont, agg, new Microsoft.Xna.Framework.Vector2(4, 2), Microsoft.Xna.Framework.Color.Black);
             sb.DrawString(Game1.smallFont, agg, new Microsoft.Xna.Framework.Vector2(4, 6), Microsoft.Xna.Framework.Color.Black);
             sb.DrawString(Game1.smallFont, agg, new Microsoft.Xna.Framework.Vector2(2, 4), Microsoft.Xna.Framework.Color.Black);
@@ -32,18 +44,18 @@ namespace SplitscreenImproved.MusicFix
             sb.DrawString(Game1.smallFont, agg, new Microsoft.Xna.Framework.Vector2(4, 4), Microsoft.Xna.Framework.Color.White);
         }
 
-        private static readonly FieldInfo activeMusicContextField = AccessTools.DeclaredField(typeof(Game1), "_instanceActiveMusicContext");
+        private static readonly FieldInfo instanceActiveMusicContextField = AccessTools.DeclaredField(typeof(Game1), "_instanceActiveMusicContext");
         internal static bool IsMusicContextActiveButNotPlaying(MusicContext music_context)
         {
-            Game1 instance = Game1.game1;
-            MusicContext activeMusicContext = (MusicContext)activeMusicContextField.GetValue(instance);
+            Game1 thisInstance = Game1.game1;
+            MusicContext this_activeMusicContext = (MusicContext)instanceActiveMusicContextField.GetValue(thisInstance);
 
-            // TODO: Check if this is not main instance, and if so, perform separate logic.
+            Game1 mainInstance = GameRunner.instance.gameInstances.Single(x => x.IsMainInstance);
 
-            // DEBUG
-            if (!instance.IsMainInstance)
+            // Check if this is not main instance, and if so, perform separate logic.
+            if (!thisInstance.IsMainInstance)
             {
-                if (activeMusicContext != music_context)
+                if (this_activeMusicContext != music_context)
                 {
                     return false;
                 }
@@ -51,16 +63,25 @@ namespace SplitscreenImproved.MusicFix
                 {
                     return false;
                 }
-                string splitscreen_currentTrack = Game1.getMusicTrackName(MusicContext.ImportantSplitScreenMusic);
-                if (splitscreen_currentTrack == "none")
+
+                //DEBUG
+                string thisCurrentTrack = Game1.getMusicTrackName(music_context);
+                if (thisCurrentTrack != "none")
                 {
-                    splitscreen_currentTrack = Game1.getMusicTrackName(music_context);
-                    if (splitscreen_currentTrack == "none")
+
+                }
+                //DEBUG
+
+                string mainCurrentTrack = GetMusicTrackNameOfInstance(mainInstance, music_context);
+                if (mainCurrentTrack == "none")
+                {
+                    mainCurrentTrack = GetMusicTrackNameOfInstance(mainInstance, music_context);
+                    if (mainCurrentTrack == "none")
                     {
                         return true;
                     }
                 }
-                if (Game1.currentSong != null && Game1.currentSong.Name == splitscreen_currentTrack && !Game1.currentSong.IsPlaying)
+                if (Game1.currentSong != null && Game1.currentSong.Name == mainCurrentTrack && !Game1.currentSong.IsPlaying)
                 {
                     return true;
                 }
@@ -69,7 +90,7 @@ namespace SplitscreenImproved.MusicFix
             // DEBUG
 
             // Base game logic.
-            if (activeMusicContext != music_context)
+            if (this_activeMusicContext != music_context)
             {
                 return false;
             }
@@ -89,8 +110,26 @@ namespace SplitscreenImproved.MusicFix
             return false;
         }
 
+        // Refactored base game method to include a Game1 instance parameter.
+        private static readonly FieldInfo instanceRequestedMusicTracksField = AccessTools.DeclaredField(typeof(Game1), "_instanceRequestedMusicTracks");
+        public static string GetMusicTrackNameOfInstance(Game1 instance, MusicContext music_context = MusicContext.Default)
+        {
+            var requestedMusicTracks = (Dictionary<MusicContext, KeyValuePair<string, bool>>)instanceRequestedMusicTracksField.GetValue(instance);
+
+            if (requestedMusicTracks.TryGetValue(music_context, out var value))
+            {
+                return value.Key;
+            }
+
+            if (music_context == MusicContext.Default)
+            {
+                return GetMusicTrackNameOfInstance(instance, MusicContext.SubLocation);
+            }
+
+            return "none";
+        }
+
         /*
-        private static readonly FieldInfo requestedMusicTracksField = AccessTools.DeclaredField(typeof(Game1), "_instanceRequestedMusicTracks");
 
         internal static void SetActiveMusicContextForImportantSplitScreenMusic()
         {
