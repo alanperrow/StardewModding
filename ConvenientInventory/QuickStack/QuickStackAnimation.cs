293fd95ead8a6a235989bc727c96852fa16cd42a
Alan Perrow <65362286+alanperrow@users.noreply.github.com>
2024-07-06 15:58:01 -0400

    Removed debug code in QuickStackAnimation. Created a Dictionary<Chest, int> so each chest can have its own count, so that item delay is calculated per-chest.

--- ConvenientInventory/QuickStack/QuickStackAnimation.cs
+++ ConvenientInventory/QuickStack/QuickStackAnimation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewValley.ItemTypeDefinitions;
@@ -19,14 +20,15 @@ namespace ConvenientInventory.QuickStack
         public QuickStackAnimation(Farmer who)
         {
             Farmer = who;
-            ItemSprites = new();
         }
 
-        public int NumItemsAnimated { get; private set; }
+        public int NumAnimatedItems { get; private set; }
 
-        private TemporaryAnimatedSpriteList ItemSprites { get; }
+        private TemporaryAnimatedSpriteList ItemSprites { get; } = new();
 
         private Farmer Farmer { get; }
+
+        private Dictionary<Chest, int> NumAnimatedItemsByChest { get; } = new();
 
         /// <summary>
         /// Broadcasts item sprites to begin animation, synced with multiplayer.
@@ -36,7 +38,7 @@ namespace ConvenientInventory.QuickStack
         public int Complete()
         {
             Game1.Multiplayer.broadcastSprites(Farmer.currentLocation, ItemSprites);
-            return NumItemsAnimated;
+            return NumAnimatedItems;
         }
 
         /// <summary>
@@ -46,86 +48,85 @@ namespace ConvenientInventory.QuickStack
         /// <param name="item">The item being quick stacked.</param>
         public void AddToAnimation(Chest chest, Item item)
         {
-            // TODO: Create a Dictionary<Chest, int> so each chest can have its own count, so that item delay is calculated per-chest.
-
-            // DEBUG: Currently adding random number of item stacks for testing purposes.
-            int numChestItemStacks = new Random().Next(8) + 1;
-            for (int i = 0; i < numChestItemStacks; i++)
+            if (!NumAnimatedItemsByChest.TryGetValue(chest, out int numChestAnimatedItems))
             {
-                // TODO: Junimo Hut/Fridge/Mill item stack is being tossed toward top-left of current location, near (0,0). Investigate and fix.
-                Vector2 chestPosition = (chest.TileLocation + new Vector2(0, -1.5f)) * Game1.tileSize;
-                Vector2 farmerOffset = Farmer.FacingDirection switch
-                {
-                    0 => new Vector2(0f, -1.5f) * Game1.tileSize,   // Up
-                    1 => new Vector2(0.5f, -1f) * Game1.tileSize,   // Right
-                    3 => new Vector2(-0.5f, -1f) * Game1.tileSize,  // Left
-                    _ => new Vector2(0f, -0.5f) * Game1.tileSize,   // Down
-                };
-                Vector2 farmerPosition = Farmer.Position + farmerOffset;
-
-                float distance = Vector2.Distance(farmerPosition, chestPosition);
-                Vector2 motionVec = (chestPosition - farmerPosition) * 0.98f; // 0.98 multiplier gives a better result in-game; without it, items slightly overshoot the chest.
-
-                float CONFIG_animationSpeed = 1.0f; // TODO: Implement in ModConfig: range = [0.5 to 3, 0.1 interval]; 0.5x speed (half speed) up to 3x speed (triple speed).
-                float tossTime = (float)(10 * Math.Pow(distance, 0.5)) + 400 - 0.5f * Math.Min(0, motionVec.Y) / CONFIG_animationSpeed;
-
-                float extraHeight = 192 - Math.Min(0, motionVec.Y);
-                float gravity = 2 * extraHeight / tossTime;
-                motionVec.Y -= extraHeight;
-                float extraX = motionVec.X;
-                float xAccel = -2 * motionVec.X / tossTime;
-                motionVec.X += extraX;
-
-                float baseLayerDepth = (float)((chest.TileLocation.Y + 1) * 64) / 10000f + chest.TileLocation.X / 50000f; // Refactored from Object.draw()
-                float addlayerDepth = 1E-06f * NumItemsAnimated; // Avoids z-fighting by drawing each sprite on a separate layer depth.
-
-                int delayPerItem = 0;
-
-                float CONFIG_animationHoverSpeed = 1.0f; // TODO: Implement in ModConfig: range = [0.5 to 3, 0.1 interval]; 0.5x speed (half speed) up to 3x speed (triple speed).
-                int hoverTimePerItem = (int)(150 / CONFIG_animationHoverSpeed);
-                int fadeTime = (int)(500 / CONFIG_animationHoverSpeed);
-
-                ParsedItemData itemData = ItemRegistry.GetDataOrErrorItem("(O)" + new Random().Next(100)); // DEBUG: Get random item sprite.
-                //ParsedItemData itemData = ItemRegistry.GetDataOrErrorItem(item.QualifiedItemId);
-                var itemTossSprite = new TemporaryAnimatedSprite(itemData.GetTextureName(), itemData.GetSourceRect(), farmerPosition, false, alphaFade: 0f, Color.White)
-                {
-                    delayBeforeAnimationStart = i * delayPerItem,
-                    scale = 4f,
-                    layerDepth = 1f - addlayerDepth,
-                    totalNumberOfLoops = 0,
-                    interval = tossTime,
-                    motion = motionVec / tossTime,
-                    acceleration = new Vector2(xAccel, gravity) / tossTime,
-                    timeBasedMotion = true,
-                };
-                var itemHoverSprite = new TemporaryAnimatedSprite(itemData.GetTextureName(), itemData.GetSourceRect(), chestPosition, false, alphaFade: 0f, Color.White)
-                {
-                    delayBeforeAnimationStart = i * delayPerItem + (int)tossTime,
-                    scale = 4f,
-                    layerDepth = baseLayerDepth - addlayerDepth,
-                    interval = i * hoverTimePerItem,
-                };
-                var itemFadeSprite = new TemporaryAnimatedSprite(itemData.GetTextureName(), itemData.GetSourceRect(), chestPosition, false, alphaFade: 0f, Color.White)
-                {
-                    delayBeforeAnimationStart = i * delayPerItem + (int)tossTime + i * hoverTimePerItem,
-                    scale = 4f,
-                    layerDepth = baseLayerDepth + addlayerDepth,
-                    alphaFade = 0.04f / CONFIG_animationHoverSpeed,
-                    interval = fadeTime,
-                    motion = new Vector2(0.6f, 4.5f) / CONFIG_animationHoverSpeed,
-                    acceleration = new Vector2(0f, -0.08f) / CONFIG_animationHoverSpeed,
-                    scaleChange = -0.07f / CONFIG_animationHoverSpeed,
-                };
-
-                ItemSprites.Add(itemTossSprite);
-                ItemSprites.Add(itemHoverSprite);
-                ItemSprites.Add(itemFadeSprite);
-
-                NumItemsAnimated++;
-
-                int totalAnimationTimeMs = (i * delayPerItem + (int)tossTime + i * hoverTimePerItem + fadeTime);
-                QuickStackChestAnimation.SetModData(chest, totalAnimationTimeMs);
+                numChestAnimatedItems = 0;
+                NumAnimatedItemsByChest[chest] = 0;
             }
+
+            // TODO: Junimo Hut/Fridge/Mill item stack is being tossed toward top-left of current location, near (0,0). Investigate and fix.
+            Vector2 chestPosition = (chest.TileLocation + new Vector2(0, -1.5f)) * Game1.tileSize;
+            Vector2 farmerOffset = Farmer.FacingDirection switch
+            {
+                0 => new Vector2(0f, -1.5f) * Game1.tileSize,   // Up
+                1 => new Vector2(0.5f, -1f) * Game1.tileSize,   // Right
+                3 => new Vector2(-0.5f, -1f) * Game1.tileSize,  // Left
+                _ => new Vector2(0f, -0.5f) * Game1.tileSize,   // Down
+            };
+            Vector2 farmerPosition = Farmer.Position + farmerOffset;
+
+            float distance = Vector2.Distance(farmerPosition, chestPosition);
+            Vector2 motionVec = (chestPosition - farmerPosition) * 0.98f; // 0.98 multiplier gives a better result in-game; without it, items slightly overshoot the chest.
+
+            float CONFIG_animationSpeed = 1.0f; // TODO: Implement in ModConfig: range = [0.5 to 3, 0.1 interval]; 0.5x speed (half speed) up to 3x speed (triple speed).
+            float tossTime = (float)(10 * Math.Pow(distance, 0.5)) + 400 - 0.5f * Math.Min(0, motionVec.Y) / CONFIG_animationSpeed;
+
+            float extraHeight = 192 - Math.Min(0, motionVec.Y);
+            float gravity = 2 * extraHeight / tossTime;
+            motionVec.Y -= extraHeight;
+            float extraX = motionVec.X;
+            float xAccel = -2 * motionVec.X / tossTime;
+            motionVec.X += extraX;
+
+            float baseLayerDepth = (float)((chest.TileLocation.Y + 1) * 64) / 10000f + chest.TileLocation.X / 50000f; // Refactored from Object.draw()
+            float addlayerDepth = 1E-06f * NumAnimatedItems; // Avoids z-fighting by drawing each sprite on a separate layer depth.
+
+            int delayPerItem = 0;
+
+            float CONFIG_animationHoverSpeed = 1.0f; // TODO: Implement in ModConfig: range = [0.5 to 3, 0.1 interval]; 0.5x speed (half speed) up to 3x speed (triple speed).
+            int hoverTimePerItem = (int)(150 / CONFIG_animationHoverSpeed);
+            int fadeTime = (int)(500 / CONFIG_animationHoverSpeed);
+
+            ParsedItemData itemData = ItemRegistry.GetDataOrErrorItem(item.QualifiedItemId);
+            var itemTossSprite = new TemporaryAnimatedSprite(itemData.GetTextureName(), itemData.GetSourceRect(), farmerPosition, false, alphaFade: 0f, Color.White)
+            {
+                delayBeforeAnimationStart = numChestAnimatedItems * delayPerItem,
+                scale = 4f,
+                layerDepth = 1f - addlayerDepth,
+                totalNumberOfLoops = 0,
+                interval = tossTime,
+                motion = motionVec / tossTime,
+                acceleration = new Vector2(xAccel, gravity) / tossTime,
+                timeBasedMotion = true,
+            };
+            var itemHoverSprite = new TemporaryAnimatedSprite(itemData.GetTextureName(), itemData.GetSourceRect(), chestPosition, false, alphaFade: 0f, Color.White)
+            {
+                delayBeforeAnimationStart = itemTossSprite.delayBeforeAnimationStart + (int)itemTossSprite.interval,
+                scale = 4f,
+                layerDepth = baseLayerDepth - addlayerDepth,
+                interval = numChestAnimatedItems * hoverTimePerItem,
+            };
+            var itemFadeSprite = new TemporaryAnimatedSprite(itemData.GetTextureName(), itemData.GetSourceRect(), chestPosition, false, alphaFade: 0f, Color.White)
+            {
+                delayBeforeAnimationStart = itemHoverSprite.delayBeforeAnimationStart + (int)itemHoverSprite.interval,
+                scale = 4f,
+                layerDepth = baseLayerDepth + addlayerDepth,
+                alphaFade = 0.04f / CONFIG_animationHoverSpeed,
+                interval = fadeTime,
+                motion = new Vector2(0.6f, 4.5f) / CONFIG_animationHoverSpeed,
+                acceleration = new Vector2(0f, -0.08f) / CONFIG_animationHoverSpeed,
+                scaleChange = -0.07f / CONFIG_animationHoverSpeed,
+            };
+
+            ItemSprites.Add(itemTossSprite);
+            ItemSprites.Add(itemHoverSprite);
+            ItemSprites.Add(itemFadeSprite);
+
+            NumAnimatedItems++;
+            NumAnimatedItemsByChest[chest]++;
+
+            int totalAnimationTimeMs = itemFadeSprite.delayBeforeAnimationStart + (int)itemFadeSprite.interval;
+            QuickStackChestAnimation.SetModData(chest, totalAnimationTimeMs);
         }
     }
 }
