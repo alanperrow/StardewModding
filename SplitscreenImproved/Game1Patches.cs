73ab770d0d35452dcd3053c1b5f7664638fa7bcf
Alan Perrow <65362286+alanperrow@users.noreply.github.com>
2025-01-21 16:39:37 -0500

    SetWindowSize_Transpiler is working for using custom screen splits.

--- SplitscreenImproved/Game1Patches.cs
+++ SplitscreenImproved/Game1Patches.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using HarmonyLib;
+using Microsoft.Xna.Framework;
 using SplitscreenImproved.Layout;
 using SplitscreenImproved.MusicFix;
 using StardewModdingAPI;
@@ -11,6 +16,101 @@ namespace SplitscreenImproved
     [HarmonyPatch(typeof(Game1))]
     public class Game1Patches
     {
+        /*
+        public static MethodInfo Window_ClientSizeChanged__SetWindowSize_InlinedMethod;
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(Game1.Window_ClientSizeChanged))]
+        public static IEnumerable<CodeInstruction> Window_ClientSizeChanged_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator ilg)
+        {
+            bool found = false;
+
+            // HACK: Find the inlined method at runtime by enumerating the instructions provided by the Harmony transpiler.
+            foreach (CodeInstruction instruction in instructions)
+            {
+                // Find instruction for loading delegate which invokes `SetWindowSize` method
+                // IL_00bc (instructionsList[?])
+                if (instruction.opcode == OpCodes.Ldftn)
+                {
+                    // Track the inlined delegate`SetWindowSize` method
+                    Window_ClientSizeChanged__SetWindowSize_InlinedMethod = (MethodInfo)instruction.operand;
+                    found = true;
+                }
+
+                yield return instruction;
+            }
+
+            if (!found)
+            {
+                ModEntry.Instance.Monitor.Log(
+                    $"{nameof(Game1Patches)}.{nameof(Window_ClientSizeChanged_Transpiler)} could not find target instruction(s) in " +
+                    $"{nameof(Game1.Window_ClientSizeChanged)}, so no changes were made.",
+                    LogLevel.Error);
+            }
+
+            yield break;
+        }
+        */
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(Game1.SetWindowSize))]
+        public static IEnumerable<CodeInstruction> SetWindowSize_Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo getScreenSplitMethod = AccessTools.DeclaredMethod(typeof(Game1Patches), nameof(GetScreenSplit));
+
+            bool patchApplied = false;
+            List<CodeInstruction> instructionsList = instructions.ToList();
+            for (int i = 0; i < instructionsList.Count; i++)
+            {
+                // Find instruction for `Vector4 current_screen_split = screen_splits[Game1.game1.instanceIndex];`
+                // IL_0316 (instructionsList[?])
+                if (!patchApplied
+                    && i > 3 && i < instructionsList.Count
+                    && instructionsList[i - 4].opcode == OpCodes.Ldloc_1
+                    && instructionsList[i - 3].opcode == OpCodes.Ldsfld
+                    && instructionsList[i - 2].opcode == OpCodes.Ldfld
+                    && instructionsList[i - 1].opcode == OpCodes.Callvirt
+                    && instructionsList[i].opcode == OpCodes.Stloc_2)
+                {
+                    // NOTE: At this point, we have the result of `screen_splits[Game1.game1.instanceIndex]` on the eval stack.
+                    yield return new CodeInstruction(OpCodes.Call, getScreenSplitMethod); // call helper method `GetScreenSplit`
+
+                    patchApplied = true;
+                }
+
+                yield return instructionsList[i];
+            }
+
+            if (!patchApplied)
+            {
+                ModEntry.Instance.Monitor.Log(
+                    $"{nameof(Game1Patches)}.{nameof(SetWindowSize_Transpiler)} could not find target instruction(s) in {nameof(Game1.SetWindowSize)}, so no changes were made.",
+                    LogLevel.Error);
+            }
+
+            yield break;
+        }
+
+
+        /// <summary>
+        /// Calculates the custom screen split for the current game instance, dependent on the current layout.
+        /// </summary>
+        /// <param name="originalScreenSplit">The original screen split.</param>
+        /// <returns>The screen split.</returns>
+        private static Vector4 GetScreenSplit(Vector4 originalScreenSplit)
+        {
+            if (!ModEntry.Config.IsModEnabled
+                || !ModEntry.Config.LayoutFeature.IsFeatureEnabled)
+            {
+                return originalScreenSplit;
+            }
+
+            SplitscreenLayout currentLayout = ModEntry.Config.LayoutFeature.GetSplitscreenLayoutByPreset(ModEntry.Config.LayoutFeature.PresetChoice);
+            return currentLayout.GetScreenSplits(GameRunner.instance.gameInstances.Count)[Game1.game1.instanceIndex];
+        }
+
+
+        /*
         [HarmonyPrefix]
         [HarmonyPatch(nameof(Game1.SetWindowSize))]
         public static bool SetWindowSize_Prefix(Game1 __instance, int w, int h)
@@ -34,6 +134,7 @@ namespace SplitscreenImproved
 
             return true;
         }
+        */
 
         [HarmonyPostfix]
         [HarmonyPatch(nameof(Game1.isMusicContextActiveButNotPlaying))]
