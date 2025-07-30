2b05548c94e09cca7cd0c5ca143ebdb20bcd7f62
Alan Perrow <65362286+alanperrow@users.noreply.github.com>
2025-07-29 22:02:46 -0400

    More work on flowing button placement downward. Considering removing this, and just having button always be next to fill stacks button.

--- ConvenientInventory/QuickStack/ToggleChestQuickStackLogic.cs
+++ ConvenientInventory/QuickStack/ToggleChestQuickStackLogic.cs
@@ -36,12 +36,12 @@ namespace ConvenientInventory.QuickStack
 
         private static ClickableTextureComponent CreateToggleChestQuickStackButton(ItemGrabMenu itemGrabMenu)
         {
-            ButtonPosition buttonPosition = FindBestButtonPosition(itemGrabMenu);
-            Rectangle buttonBounds = GetBoundsByButtonPosition(buttonPosition, itemGrabMenu);
+            MenuButtons menuButtons = GetMenuButtons(itemGrabMenu);
+            ToggleQuickStackButtonPosition buttonPosition = FindBestPositionForToggleQuickStackButton(menuButtons);
 
-            ClickableTextureComponent button = new(
+            ClickableTextureComponent toggleQuickStackButton = new(
                 name: string.Empty,
-                bounds: buttonBounds,
+                bounds: GetBoundsByButtonPosition(buttonPosition, itemGrabMenu),
                 label: string.Empty,
                 hoverText: GetButtonHoverText(itemGrabMenu),
                 texture: GetButtonTexture(itemGrabMenu),
@@ -51,36 +51,35 @@ namespace ConvenientInventory.QuickStack
                 myID = ToggleChestQuickStackButtonID,
             };
 
-            // TODO: update neighbor IDs based on button position
-
-            //downNeighborID = InventoryPage.region_trashCan,
-            //upNeighborID = InventoryPage.region_organizeButton,
-            //leftNeighborID = 11, // top-right inventory slot
-
-            return button;
+            SetNeighborIdsForToggleQuickStackButton(toggleQuickStackButton, itemGrabMenu, menuButtons, buttonPosition);
+            return toggleQuickStackButton;
         }
 
-        private enum ButtonPosition
+        private static MenuButtons GetMenuButtons(ItemGrabMenu itemGrabMenu)
         {
-            RightOfFillStacks,
-            FillStacks,
-            ColorPickerOrSpecial,
-            JunimoNote,
+            MenuButtons buttons = MenuButtons.None;
+            buttons |= (itemGrabMenu.junimoNoteIcon != null) ? MenuButtons.JunimoNote : MenuButtons.None;
+            buttons |= (itemGrabMenu.colorPickerToggleButton != null) ? MenuButtons.ColorPickerToggle : MenuButtons.None;
+            buttons |= (itemGrabMenu.specialButton != null) ? MenuButtons.Special : MenuButtons.None;
+            buttons |= (itemGrabMenu.fillStacksButton != null) ? MenuButtons.FillStacks : MenuButtons.None;
+            buttons |= (itemGrabMenu.organizeButton != null) ? MenuButtons.Organize : MenuButtons.None;
+
+            return buttons;
         }
 
         /// <summary>
         /// Finds the best valid Toggle Quick Stack button position (i.e. no button already there) for the provided <paramref name="itemGrabMenu"/>.
         /// </summary>
-        private static ButtonPosition FindBestButtonPosition(ItemGrabMenu itemGrabMenu) => itemGrabMenu switch
+        private static ToggleQuickStackButtonPosition FindBestPositionForToggleQuickStackButton(MenuButtons menuButtons) => menuButtons switch
         {
             // Ideal position is where Junimo Note icon would otherwise be; if not valid, flow downwards, defaulting to the right of Fill Stacks.
-            _ when itemGrabMenu.junimoNoteIcon is null => ButtonPosition.JunimoNote,
-            _ when itemGrabMenu.colorPickerToggleButton is null && itemGrabMenu.specialButton is null => ButtonPosition.ColorPickerOrSpecial,
-            _ when itemGrabMenu.fillStacksButton is null => ButtonPosition.FillStacks,
-            _ => ButtonPosition.RightOfFillStacks,
+            _ when !menuButtons.HasFlag(MenuButtons.JunimoNote) => ToggleQuickStackButtonPosition.JunimoNote,
+            _ when !menuButtons.HasFlag(MenuButtons.ColorPickerToggle) && !menuButtons.HasFlag(MenuButtons.Special) => ToggleQuickStackButtonPosition.ColorPickerOrSpecial,
+            _ when !menuButtons.HasFlag(MenuButtons.FillStacks) => ToggleQuickStackButtonPosition.FillStacks,
+            _ => ToggleQuickStackButtonPosition.RightOfFillStacks,
         };
 
-        private static Rectangle GetBoundsByButtonPosition(ButtonPosition buttonPosition, ItemGrabMenu itemGrabMenu)
+        private static Rectangle GetBoundsByButtonPosition(ToggleQuickStackButtonPosition buttonPosition, ItemGrabMenu itemGrabMenu)
         {
             // Ideal position for button is where Junimo Note icon would otherwise be.
             int buttonX = itemGrabMenu.xPositionOnScreen + itemGrabMenu.width;
@@ -90,17 +89,17 @@ namespace ConvenientInventory.QuickStack
 
             switch (buttonPosition)
             {
-                case ButtonPosition.JunimoNote:
+                case ToggleQuickStackButtonPosition.JunimoNote:
                     break;
-                case ButtonPosition.ColorPickerOrSpecial:
+                case ToggleQuickStackButtonPosition.ColorPickerOrSpecial:
                     // Use y-position of the color picker toggle/special button.
                     buttonY = itemGrabMenu.yPositionOnScreen + itemGrabMenu.height / 3 - 64 - 160;
                     break;
-                case ButtonPosition.FillStacks:
+                case ToggleQuickStackButtonPosition.FillStacks:
                     // Use y-position of the fill stacks button.
                     buttonY = itemGrabMenu.yPositionOnScreen + itemGrabMenu.height / 3 - 64 - 64 - 16;
                     break;
-                case ButtonPosition.RightOfFillStacks:
+                case ToggleQuickStackButtonPosition.RightOfFillStacks:
                     // Use y-position of the fill stacks button, and shift to the right.
                     buttonY = itemGrabMenu.yPositionOnScreen + itemGrabMenu.height / 3 - 64 - 64 - 16;
                     buttonX += buttonWidth + 16;
@@ -126,6 +125,53 @@ namespace ConvenientInventory.QuickStack
         {
             // TODO: Dynamically get texture based on modData
             return CachedTextures.ChestQuickStackEnabledButtonIcon;
+        }
+
+        private static void SetNeighborIdsForToggleQuickStackButton(
+            ClickableTextureComponent toggleQuickStackButton,
+            ItemGrabMenu itemGrabMenu,
+            MenuButtons menuButtons,
+            ToggleQuickStackButtonPosition buttonPosition)
+        {
+            //downNeighborID = InventoryPage.region_trashCan,
+            //upNeighborID = InventoryPage.region_organizeButton,
+            //leftNeighborID = 11, // top-right inventory slot
+
+            // TODO: Need to Postfix `ItemGrabMenu.SetupBorderNeighbors` to accommodate:
+            //          1) if color picker is toggled:
+            //              - if we are in Junimo Note's spot, this will require our button's top neighbor to be updated
+            //          2) if no empty button space available and we had to place our button to the right of fill stacks, we 
+
+            // ACTUALLY
+            // Would it simpler if we just always placed the button to the right of the fill stacks button?
+            // All supported chests for quick stack have a fill stacks button, so there wouldn't be any empty spaces.
+            // Looks a little uglier than filling in the empty Junimo Note space, but it also is better UX to have the button alwys be in the same position.
+            // I'm leaning toward this idea...
+
+            switch (buttonPosition)
+            {
+                case ToggleQuickStackButtonPosition.JunimoNote:
+
+            }
+        }
+
+        [Flags]
+        private enum MenuButtons
+        {
+            None = 0,
+            JunimoNote = 1 << 0,
+            ColorPickerToggle = 1 << 1,
+            Special = 1 << 2,
+            FillStacks = 1 << 3,
+            Organize = 1 << 4,
+        }
+
+        private enum ToggleQuickStackButtonPosition
+        {
+            RightOfFillStacks,
+            FillStacks,
+            ColorPickerOrSpecial,
+            JunimoNote,
         }
     }
 }
