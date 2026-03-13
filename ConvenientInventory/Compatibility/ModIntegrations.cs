adb32d76216e5c34be4466a460a57b54ca2d628a
Alan Perrow <65362286+alanperrow@users.noreply.github.com>
2026-03-12 22:06:53 -0400

    Reformatted ending parentheses

--- ConvenientInventory/Compatibility/ModIntegrations.cs
+++ ConvenientInventory/Compatibility/ModIntegrations.cs
@@ -42,24 +42,19 @@ namespace ConvenientInventory.Compatibility
             api.Register(
                 mod: modManifest,
                 reset: config.Reset,
-                save: config.Save
-            );
+                save: config.Save);
 
             // ===== Quick Stack To Nearby Chests =====
             api.AddSectionTitle(
                 mod: modManifest,
-                text: I18n.ModConfigMenu_Label_QuickStackToNearbyChests
-            );
-
-            //TODO: Use new GMCM version to include subheaders to break up large Quick Stack section.
+                text: I18n.ModConfigMenu_Label_QuickStackToNearbyChests);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IsEnabled,
                 setValue: value => config.QuickStack.IsEnabled = value,
                 name: I18n.ModConfigMenu_IsEnableQuickStack_Name,
-                tooltip: I18n.ModConfigMenu_IsEnableQuickStack_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsEnableQuickStack_Desc);
 
             api.AddNumberOption(
                 mod: modManifest,
@@ -70,152 +65,137 @@ namespace ConvenientInventory.Compatibility
                 min: 1,
                 max: IsChestsAnywhereInstalled ? ConfigHelper.QuickStackRange_GlobalInt : ConfigHelper.QuickStackRange_LocationInt,
                 interval: 1,
-                formatValue: ConfigHelper.FormatQuickStackRange
-            );
+                formatValue: ConfigHelper.FormatQuickStackRange);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.OverflowItems,
                 setValue: value => config.QuickStack.OverflowItems = value,
                 name: I18n.ModConfigMenu_IsQuickStackOverflowItems_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackOverflowItems_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackOverflowItems_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IgnoreItemQuality,
                 setValue: value => config.QuickStack.IgnoreItemQuality = value,
                 name: I18n.ModConfigMenu_IsQuickStackIgnoreItemQuality_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackIgnoreItemQuality_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackIgnoreItemQuality_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IsHotkeyEnabled,
                 setValue: value => config.QuickStack.IsHotkeyEnabled = value,
                 name: I18n.ModConfigMenu_IsEnableQuickStackHotkey_Name,
-                tooltip: I18n.ModConfigMenu_IsEnableQuickStackHotkey_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsEnableQuickStackHotkey_Desc);
 
             api.AddKeybindList(
                 mod: modManifest,
                 getValue: () => config.QuickStack.KeyboardHotkey,
                 setValue: value => config.QuickStack.KeyboardHotkey = value,
                 name: I18n.ModConfigMenu_QuickStackKeyboardHotkey_Name,
-                tooltip: I18n.ModConfigMenu_QuickStackKeyboardHotkey_Desc
-            );
+                tooltip: I18n.ModConfigMenu_QuickStackKeyboardHotkey_Desc);
 
             api.AddKeybindList(
                 mod: modManifest,
                 getValue: () => config.QuickStack.ControllerHotkey,
                 setValue: value => config.QuickStack.ControllerHotkey = value,
                 name: I18n.ModConfigMenu_QuickStackControllerHotkey_Name,
-                tooltip: I18n.ModConfigMenu_QuickStackControllerHotkey_Desc
-            );
+                tooltip: I18n.ModConfigMenu_QuickStackControllerHotkey_Desc);
+
+            //api.AddSubHeader(
+            //    mod: modManifest,
+            //    text: I18n.ModConfigMenu_QuickStackAdditionalSettings_Label);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.AllowHotkeyInChestMenu,
                 setValue: value => config.QuickStack.AllowHotkeyInChestMenu = value,
                 name: I18n.ModConfigMenu_IsQuickStackAllowHotkeyInChestMenu_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackAllowHotkeyInChestMenu_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackAllowHotkeyInChestMenu_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.WithFillStacksButton,
                 setValue: value => config.QuickStack.WithFillStacksButton = value,
                 name: I18n.ModConfigMenu_IsQuickStackWithFillStacksButton_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackWithFillStacksButton_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackWithFillStacksButton_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.VisuallyOverrideFillStacksButton,
                 setValue: value => config.QuickStack.VisuallyOverrideFillStacksButton = value,
                 name: I18n.ModConfigMenu_IsQuickStackVisuallyOverrideFillStacksButton_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackVisuallyOverrideFillStacksButton_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackVisuallyOverrideFillStacksButton_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IsToggleChestEnabled,
                 setValue: value => config.QuickStack.IsToggleChestEnabled = value,
                 name: I18n.ModConfigMenu_IsEnableQuickStackToggleChest_Name,
-                tooltip: I18n.ModConfigMenu_IsEnableQuickStackToggleChest_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsEnableQuickStackToggleChest_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IsPrioritizeChestEnabled,
                 setValue: value => config.QuickStack.IsPrioritizeChestEnabled = value,
                 name: I18n.ModConfigMenu_IsEnableQuickStackPrioritizeChest_Name,
-                tooltip: I18n.ModConfigMenu_IsEnableQuickStackPrioritizeChest_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsEnableQuickStackPrioritizeChest_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IsToggleChestButtonHidden,
                 setValue: value => config.QuickStack.IsToggleChestButtonHidden = value,
                 name: I18n.ModConfigMenu_IsQuickStackToggleChestButtonHidden_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackToggleChestButtonHidden_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackToggleChestButtonHidden_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IntoMills,
                 setValue: value => config.QuickStack.IntoMills = value,
                 name: I18n.ModConfigMenu_IsQuickStackIntoMills_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackIntoMills_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackIntoMills_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IntoJunimoHuts,
                 setValue: value => config.QuickStack.IntoJunimoHuts = value,
                 name: I18n.ModConfigMenu_IsQuickStackIntoJunimoHuts_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackIntoJunimoHuts_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackIntoJunimoHuts_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IntoDressers,
                 setValue: value => config.QuickStack.IntoDressers = value,
                 name: I18n.ModConfigMenu_IsQuickStackIntoDressers_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackIntoDressers_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackIntoDressers_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IntoHoppers,
                 setValue: value => config.QuickStack.IntoHoppers = value,
                 name: I18n.ModConfigMenu_IsQuickStackIntoHoppers_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackIntoHoppers_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackIntoHoppers_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.IntoMiniShippingBins,
                 setValue: value => config.QuickStack.IntoMiniShippingBins = value,
                 name: I18n.ModConfigMenu_IsQuickStackIntoMiniShippingBins_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackIntoMiniShippingBins_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackIntoMiniShippingBins_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.DrawChestsInButtonTooltip,
                 setValue: value => config.QuickStack.DrawChestsInButtonTooltip = value,
                 name: I18n.ModConfigMenu_IsQuickStackTooltipDrawNearbyChests_Name,
-                tooltip: I18n.ModConfigMenu_IsQuickStackTooltipDrawNearbyChests_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsQuickStackTooltipDrawNearbyChests_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.QuickStack.SuppressSoundWhenNoNearbyChests,
                 setValue: value => config.QuickStack.SuppressSoundWhenNoNearbyChests = value,
                 name: I18n.ModConfigMenu_IsSuppressSoundWhenNoNearbyChests_Name,
-                tooltip: I18n.ModConfigMenu_IsSuppressSoundWhenNoNearbyChests_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsSuppressSoundWhenNoNearbyChests_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
@@ -229,8 +209,7 @@ namespace ConvenientInventory.Compatibility
                     config.QuickStack.IsChestAnimationEnabled = value;
                 },
                 name: I18n.ModConfigMenu_IsEnableQuickStackAnimation_Name,
-                tooltip: I18n.ModConfigMenu_IsEnableQuickStackAnimation_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsEnableQuickStackAnimation_Desc);
 
             api.AddNumberOption(
                 mod: modManifest,
@@ -240,8 +219,7 @@ namespace ConvenientInventory.Compatibility
                 tooltip: I18n.ModConfigMenu_QuickStackAnimationItemSpeed_Desc,
                 min: 0.5f,
                 max: 3f,
-                interval: 0.1f
-            );
+                interval: 0.1f);
 
             api.AddNumberOption(
                 mod: modManifest,
@@ -251,22 +229,19 @@ namespace ConvenientInventory.Compatibility
                 tooltip: I18n.ModConfigMenu_QuickStackAnimationStackSpeed_Desc,
                 min: 0.5f,
                 max: 3f,
-                interval: 0.1f
-            );
+                interval: 0.1f);
 
             // ===== Favorite Items =====
             api.AddSectionTitle(
                 mod: modManifest,
-                text: I18n.ModConfigMenu_Label_FavoriteItems
-            );
+                text: I18n.ModConfigMenu_Label_FavoriteItems);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.FavoriteItems.IsEnabled,
                 setValue: value => config.FavoriteItems.IsEnabled = value,
                 name: I18n.ModConfigMenu_IsEnableFavoriteItems_Name,
-                tooltip: I18n.ModConfigMenu_IsEnableFavoriteItems_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsEnableFavoriteItems_Desc);
 
             string[] highlightStyleDescriptions =
             {
@@ -289,8 +264,7 @@ namespace ConvenientInventory.Compatibility
                 },
                 name: I18n.ModConfigMenu_FavoriteItemsHighlightTextureChoice_Name,
                 tooltip: I18n.ModConfigMenu_FavoriteItemsHighlightTextureChoice_Desc,
-                allowedValues: highlightStyleDescriptions
-            );
+                allowedValues: highlightStyleDescriptions);
 
             // TODO: Visualize the highlight texture choice.
             //       Probably doesn't need to update live, but see SplitscreenImproved for reference if it does.
@@ -300,82 +274,71 @@ namespace ConvenientInventory.Compatibility
                 getValue: () => config.FavoriteItems.KeyboardHotkey,
                 setValue: value => config.FavoriteItems.KeyboardHotkey = value,
                 name: I18n.ModConfigMenu_FavoriteItemsKeyboardHotkey_Name,
-                tooltip: I18n.ModConfigMenu_FavoriteItemsKeyboardHotkey_Desc
-            );
+                tooltip: I18n.ModConfigMenu_FavoriteItemsKeyboardHotkey_Desc);
 
             api.AddKeybindList(
                 mod: modManifest,
                 getValue: () => config.FavoriteItems.ControllerHotkey,
                 setValue: value => config.FavoriteItems.ControllerHotkey = value,
                 name: I18n.ModConfigMenu_FavoriteItemsControllerHotkey_Name,
-                tooltip: I18n.ModConfigMenu_FavoriteItemsControllerHotkey_Desc
-            );
+                tooltip: I18n.ModConfigMenu_FavoriteItemsControllerHotkey_Desc);
 
             // ===== Take All But One =====
             api.AddSectionTitle(
                 mod: modManifest,
-                text: I18n.ModConfigMenu_Label_TakeAllButOne
-            );
+                text: I18n.ModConfigMenu_Label_TakeAllButOne);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.TakeAllButOne.IsEnabled,
                 setValue: value => config.TakeAllButOne.IsEnabled = value,
                 name: I18n.ModConfigMenu_IsEnableTakeAllButOne_Name,
-                tooltip: I18n.ModConfigMenu_IsEnableTakeAllButOne_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsEnableTakeAllButOne_Desc);
 
             api.AddKeybindList(
                 mod: modManifest,
                 getValue: () => config.TakeAllButOne.KeyboardHotkey,
                 setValue: value => config.TakeAllButOne.KeyboardHotkey = value,
                 name: I18n.ModConfigMenu_TakeAllButOneKeyboardHotkey_Name,
-                tooltip: I18n.ModConfigMenu_TakeAllButOneKeyboardHotkey_Desc
-            );
+                tooltip: I18n.ModConfigMenu_TakeAllButOneKeyboardHotkey_Desc);
 
             api.AddKeybindList(
                 mod: modManifest,
                 getValue: () => config.TakeAllButOne.ControllerHotkey,
                 setValue: value => config.TakeAllButOne.ControllerHotkey = value,
                 name: I18n.ModConfigMenu_TakeAllButOneControllerHotkey_Name,
-                tooltip: I18n.ModConfigMenu_TakeAllButOneControllerHotkey_Desc
-            );
+                tooltip: I18n.ModConfigMenu_TakeAllButOneControllerHotkey_Desc);
 
             // ===== Auto Organize Chest =====
             api.AddSectionTitle(
                 mod: modManifest,
-                text: I18n.ModConfigMenu_Label_AutoOrganizeChest
-            );
+                text: I18n.ModConfigMenu_Label_AutoOrganizeChest);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.AutoOrganizeChest.IsEnabled,
                 setValue: value => config.AutoOrganizeChest.IsEnabled = value,
                 name: I18n.ModConfigMenu_IsEnableAutoOrganizeChest_Name,
-                tooltip: I18n.ModConfigMenu_IsEnableAutoOrganizeChest_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsEnableAutoOrganizeChest_Desc);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.AutoOrganizeChest.ShowInstructionsInTooltip,
                 setValue: value => config.AutoOrganizeChest.ShowInstructionsInTooltip = value,
                 name: I18n.ModConfigMenu_IsShowAutoOrganizeButtonInstructions_Name,
-                tooltip: I18n.ModConfigMenu_IsShowAutoOrganizeButtonInstructions_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsShowAutoOrganizeButtonInstructions_Desc);
 
             // ===== Miscellaneous =====
             api.AddSectionTitle(
                 mod: modManifest,
-                text: I18n.ModConfigMenu_Label_Miscellaneous
-            );
+                text: I18n.ModConfigMenu_Label_Miscellaneous);
 
             api.AddBoolOption(
                 mod: modManifest,
                 getValue: () => config.Miscellaneous.IsInventoryPageSideWarpEnabled,
                 setValue: value => config.Miscellaneous.IsInventoryPageSideWarpEnabled = value,
                 name: I18n.ModConfigMenu_IsEnableInventoryPageSideWarp_Name,
-                tooltip: I18n.ModConfigMenu_IsEnableInventoryPageSideWarp_Desc
-            );
+                tooltip: I18n.ModConfigMenu_IsEnableInventoryPageSideWarp_Desc);
         }
 
         /// <summary>
